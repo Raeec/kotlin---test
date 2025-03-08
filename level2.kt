44214ec581f88a0b85fac26b7d8835478986668e
Raeec <114604498+Raeec@users.noreply.github.com>
2025-03-08 16:51:14 +0500

    Create level2.kt

---
+++ level2.kt
@@ -0,0 +1,12 @@
+fun main() {
+    fun level1_1(a: Int): Boolean = (a < 0)
+
+    fun level1_2(str: String): Int = str.length
+    
+  
+  print("level1_1: ")
+    println(level1_1(-5))
+
+  print("level1_2: ")
+    println(level1_2("fsdfsfsdfs"))
+}
